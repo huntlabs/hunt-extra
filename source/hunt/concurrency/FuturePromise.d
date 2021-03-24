52681741be8f93430fd3fd3722b804cd2532f521
heromyth <bitworld@qq.com>
2021-03-24 19:44:40 +0800

    FuturePromise improved

--- source/hunt/concurrency/FuturePromise.d
+++ source/hunt/concurrency/FuturePromise.d
@@ -19,6 +19,9 @@ import hunt.logging.ConsoleLogger;
 
 import core.atomic;
 import core.thread;
+import core.sync.mutex;
+import core.sync.condition;
+
 import std.format;
 import std.datetime;
 
@@ -31,11 +34,17 @@ class FuturePromise(T) : Future!T, Promise!T {
 	private bool _isResultAvaliable = false;
 	private Exception _cause;
 	private string _id;
+	private Mutex _waiterLocker;
+	private Condition _waiterCondition;
+	private bool _isAwaiting = false;
+
 	shared static this() {
 		COMPLETED = new Exception("");
 	}
 
 	this() {
+		_waiterLocker = new Mutex(this);
+		_waiterCondition = new Condition(_waiterLocker);
 	}
 
 	string id() {
@@ -58,7 +67,8 @@ static if(is(T == void)) {
 			_cause = COMPLETED;
 			_isResultAvaliable = true;
 		} else {
-			warning("This promise has been done, and can't be set again.");
+			warningf("This promise has been done, and can't be set again. cause: %s", 
+				typeid(_cause));
 		}
 	}
 
@@ -73,9 +83,23 @@ static if(is(T == void)) {
 		if (cas(&_done, false, true)) {
 			_result = result;
 			_cause = COMPLETED;
+			
+			_waiterLocker.lock();
 			_isResultAvaliable = true;
+			scope(exit) {
+				_waiterLocker.unlock();
+			}
+
+			_waiterCondition.notifyAll();
+
+			// if(_isAwaiting) {
+			// 	_waiterCondition.notifyAll();
+			// } else {
+			// 	warning("No waiter");
+			// }
 		} else {
-			warning("This promise has been done, and can't be set again.");
+			warningf("This promise has been done, and can't be set again. cause: %s", 
+				typeid(_cause));
 		}
 	}
 	private T _result;
@@ -89,9 +113,10 @@ static if(is(T == void)) {
 	void failed(Exception cause) {
 		if (cas(&_done, false, true)) {
 			_cause = cause;
-			_isResultAvaliable = true;
+			// _isResultAvaliable = true;
 		} else {
-			warning("This promise has been done, and can't be set again.");
+			warningf("This promise has been done, and can't be set again. cause: %s", 
+				typeid(_cause));
 		}
 	}
 
@@ -101,7 +126,7 @@ static if(is(T == void)) {
 				_result = T.init;
 			}
 			_cause = new CancellationException("");
-			_isResultAvaliable = true;
+			// _isResultAvaliable = true;
 			return true;
 		}
 		return false;
@@ -126,15 +151,43 @@ static if(is(T == void)) {
 	}
 
 	T get() {
-		// waitting for the result
-		version (HUNT_DEBUG) info("Waiting for a promise...");
-		while(!_isResultAvaliable) {
-            Thread.yield();
-			version(HUNT_DANGER_DEBUG) trace("Waiting for a promise");
+		return get(-1.msecs);
+	}
+
+
+	T get(Duration timeout) {
+
+		if(_isResultAvaliable) {
+			static if(is(T == void)) {
+				return;
+			} else {
+				return _result;
+			}
 		}
 
-		version (HUNT_DEBUG) info("Got a promise");
+		// waitting for the result
+		_waiterLocker.lock();
+		if(!_isResultAvaliable) {
+			if(timeout.isNegative()) {
+				version (HUNT_DEBUG) info("Waiting for a promise...");
+				_waiterCondition.wait();
+			} else {
+				version (HUNT_DEBUG) {
+					infof("Waiting for a promise in %s...", timeout);
+				}
+				bool r = _waiterCondition.wait(timeout);
+				if(!r) {
+					debug warningf("Timeout for a promise in %s...", timeout);
+					if(!_done) {
+						failed(new TimeoutException("Timeout in " ~ timeout.toString()));					
+					}
+				}
+			}
+		}
+		_waiterLocker.unlock();
+
 		assert(_cause !is null);
+		version (HUNT_DEBUG) infof("Got a promise: %s", typeid(_cause));
 
 		if (_cause is COMPLETED) {
 			static if(is(T == void)) {
@@ -153,52 +206,52 @@ static if(is(T == void)) {
 		debug warning("Get a exception in a promise: ", _cause.msg);
 		version (HUNT_DEBUG) warning(_cause);
 		throw new ExecutionException(_cause);
-	}
+	}	
 
-	T get(Duration timeout) {
-		// waitting for the result
-		if(!_isResultAvaliable) {
-			version (HUNT_DEBUG) {
-				infof("Waiting for a promise in %s...", timeout);
-			}
-            auto start = Clock.currTime;
-            while (!_isResultAvaliable && Clock.currTime < start + timeout) {
-                Thread.yield();
-            }
+	// T get(Duration timeout) {
+	// 	// waitting for the result
+	// 	if(!_isResultAvaliable) {
+	// 		version (HUNT_DEBUG) {
+	// 			infof("Waiting for a promise in %s...", timeout);
+	// 		}
+    //         auto start = Clock.currTime;
+    //         while (!_isResultAvaliable && Clock.currTime < start + timeout) {
+    //             Thread.yield();
+    //         }
 
-			if (!_done && !_isResultAvaliable) {
-				debug warningf("Timeout for a promise in %s...", timeout);
-				failed(new TimeoutException("Timeout in " ~ timeout.toString()));
-            }
+	// 		if (!_done && !_isResultAvaliable) {
+	// 			debug warningf("Timeout for a promise in %s...", timeout);
+	// 			failed(new TimeoutException("Timeout in " ~ timeout.toString()));
+    //         }
 
-			version (HUNT_DEBUG) {
-				auto dur = Clock.currTime - start;
-				if(dur > 5.seconds) {
-					warningf("Got a promise in %s", dur);
-				} else {
-					// infof("Got a promise in %s", dur);
-				}
-			}
-		}		
+	// 		version (HUNT_DEBUG) {
+	// 			auto dur = Clock.currTime - start;
+	// 			if(dur > 5.seconds) {
+	// 				warningf("Got a promise in %s", dur);
+	// 			} else {
+	// 				// infof("Got a promise in %s", dur);
+	// 			}
+	// 		}
+	// 	}		
 
-		if (_cause is COMPLETED) {
-			static if(is(T == void)) {
-				return;
-			} else {
-				return _result;
-			}
-		}
+	// 	if (_cause is COMPLETED) {
+	// 		static if(is(T == void)) {
+	// 			return;
+	// 		} else {
+	// 			return _result;
+	// 		}
+	// 	}
 
-		TimeoutException t = cast(TimeoutException) _cause;
-		if (t !is null)
-			throw t;
+	// 	TimeoutException t = cast(TimeoutException) _cause;
+	// 	if (t !is null)
+	// 		throw t;
 
-		CancellationException c = cast(CancellationException) _cause;
-		if (c !is null)
-			throw c;
+	// 	CancellationException c = cast(CancellationException) _cause;
+	// 	if (c !is null)
+	// 		throw c;
 
-		throw new ExecutionException(_cause.msg);
-	}
+	// 	throw new ExecutionException(_cause.msg);
+	// }
 
 	override string toString() {
 		static if(is(T == void)) {
